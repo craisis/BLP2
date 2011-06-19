66c2fc78a141fe677111da3efceb7758cf106d2b
David Stearns <stearns@dhyw.com>
2011-06-19 14:09:59 -0600

    Added SHA-1 code, generate transaction and send in LogbookManager via SocketManager.  Generate correct IDs

--- public/app.js
+++ public/app.js
@@ -10,6 +10,13 @@ Ext.application({
   requires: ['BLP2.SocketManager'],
   launch: function(){
     var socket = BLP2.SocketManager.connect();
+    if(window.localStorage.getItem('originId') == undefined){
+      var oID = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+          var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+          return v.toString(16);
+      });
+      window.localStorage.setItem('originId', oID);
+    }
     BLP2.SocketManager.register('debug', {
       echo: function(data){ console.log(data); },
       alert: function(data){ alert(data.body); }
--- public/app/LogbookManager.js
+++ public/app/LogbookManager.js
@@ -3,7 +3,9 @@ Ext.define('BLP2.LogbookManager', {
   requires: [
     'BLP2.ContestManager',
     'Ext.data.Store',
-    'BLP2.SocketManager'
+    'BLP2.SocketManager',
+    'Ext.JSON',
+    'Ext.StoreManager'
   ],
   constructor: function(){
     this.logbookStores = {};
@@ -42,6 +44,26 @@ Ext.define('BLP2.LogbookManager', {
     payload.logbookId = this.currentLogbookId;
     payload.body = {};
     // Generate rest of transaction here
+    payload.body.data = {};
+    payload.body.data.contact = formData;
+    var peopleStore = Ext.StoreManager.lookup('People');
+    var operator = peopleStore.getById(formData.operatorId);
+    var logger = peopleStore.getById(formData.loggerId);
+    payload.body.data.people = {};
+    payload.body.data.people.operator = {
+      'name': operator.get('firstName') +' '+operator.get('lastName'),
+      'call-sign': operator.get('callSign')
+    };
+      payload.body.data.people.logger = {
+        'name': logger.get('firstName') +' '+logger.get('lastName'),
+        'call-sign': logger.get('callSign')
+      };
+
+    var id = Sha1.hash(Ext.JSON.encode(payload.body));
+    payload.body.ID = id;
+    payload.body['root-ID'] = id;
+    payload.body['origin-ID'] = window.localStorage.getItem('originId');
+    payload.body.timestamp = (new Date).getTime();
     BLP2.SocketManager.connect().send(payload);
   }
   
